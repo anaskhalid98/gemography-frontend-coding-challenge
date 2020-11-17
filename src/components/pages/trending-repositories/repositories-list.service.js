5867b7a5be1959224693d50cedeb5b54a3f4f4f6
AnasKhalid <anass.khalid5@gmail.com>
2020-11-17 22:12:01 +0100

    Fix getMostStarredRepositories to get repositories that were created in the last 30 days

--- src/components/pages/trending-repositories/repositories-list.service.js
+++ src/components/pages/trending-repositories/repositories-list.service.js
@@ -1,10 +1,13 @@
 import {request} from "../../../config/axios-config";
 import {GITHUB_API_URL} from "../../../constants";
+import {getCurrentDateSubtractDays, getFormattedDate} from "../../utils";
 
 
 export function getMostStarredRepositories(page) {
+	let created_date = getFormattedDate(getCurrentDateSubtractDays(30)).toString();
+
 	return request({
-		url: GITHUB_API_URL + `/search/repositories?q=created:>2017-10-22&sort=stars&order=desc&page=${page}`,
+		url: GITHUB_API_URL + `/search/repositories?q=created:>${created_date}&sort=stars&order=desc&page=${page}`,
 		method: "get",
 	});
 }
--- src/components/utils/index.js
+++ src/components/utils/index.js
@@ -1,5 +1,13 @@
-export  const  getTimeIntervalInDay = (creationDate, currentDate) => {
+export const getTimeIntervalInDay = (creationDate, currentDate) => {
 	let difference_in_time = currentDate.getTime() - creationDate.getTime();
 	let difference_in_day = Math.floor(difference_in_time / (1000 * 3600 * 24));
 	return difference_in_day;
+}
+
+export const getCurrentDateSubtractDays = (days) => {
+	return new Date(new Date().setDate(new Date().getDate() - days));
+}
+
+export const getFormattedDate = (date) => {
+	return date.getFullYear()+"-"+(date.getMonth()+1)+"-"+date.getDate();
 }
